b65a13ed434c4e3553a9d3b1fcf4e4674ea4cc22
Juan Beldi <juan.beldi@mediamonks.com>
2020-05-04 15:35:28 -0300

    Handle error when page_content is not found and go home when URL is not valid

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,16 @@ import { routes } from '../views/index.js';
 import { parseRequestURL } from '../utils/parseUrl.js';
 
 const goToRoute = async () => {
-    const content = document.getElementById('page_content') || null;
+    const content = document.getElementById('page_content');
+
+    if(!content) {
+        console.log('Page Content not found');
+        return;
+    } 
+
     const request = parseRequestURL();
     const parsedURL = '/' + request.resource;
-    const view = routes[parsedURL]() || routes['home']();
+    const view = routes[parsedURL] ? routes[parsedURL]() : routes['/home']();
 
     content.innerHTML = await view.render;
     await view.mounted && view.mounted();
