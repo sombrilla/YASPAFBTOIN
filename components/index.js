d1c6b54c21dc967995d8a46702d6e75a186bf089
Juan Beldi <juaan.beldi@hotmail.com>
2020-05-07 13:19:10 -0300

    add back lower cased components

---
+++ components/index.js
@@ -0,0 +1,21 @@
+import { Home } from './pages/home/home.js';
+import { Projects } from './pages/projects/projects.js';
+import { Contact } from './pages/contact/contact.js';
+import { Navigation } from './navigation/navigation.js';
+import { Background } from './background/background.js';
+
+export const componentsPath = './components';
+
+export const components = {
+    home: { name: 'home', component: Home },
+    projects: { name: 'projects', component: Projects },
+    contact: { name: 'contact', component: Contact },
+    navigation: { name: 'navigation', component: Navigation },
+    background: { name: 'background', component: Background },
+}
+
+export const themeColors = {
+    home: '#0af',
+    projects: 'red',
+    contact: 'green'
+};
