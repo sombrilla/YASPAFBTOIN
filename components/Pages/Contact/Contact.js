2ed5b8ec5cb6481532ad419c7a3a6c61a9787323
Juan Beldi <juaan.beldi@hotmail.com>
2020-05-07 13:17:12 -0300

    remove uppercased components

--- components/Pages/Contact/Contact.js
+++
@@ -1,8 +0,0 @@
-import { Page } from '../../../lib/Page.js';
-import { themeColors } from '../../index.js';
-
-export class Contact extends Page {
-    mounted() {
-        document.documentElement.style.setProperty('--theme-color', themeColors.contact);
-    }
-};
