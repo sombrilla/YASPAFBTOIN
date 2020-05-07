6dcaead795effe6cb7b7b8b719bf1a6aab7733c7
Juan Beldi <juaan.beldi@hotmail.com>
2020-05-07 13:48:55 -0300

    Update navigation links routing logic

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -1,9 +1,21 @@
 import { Component } from '../../lib/Component.js';
+// import { app } from '../../App.js';
 
 export class Navigation extends Component {
     constructor() {
         super();
     }
     
-    mounted() {}
+    mounted() {
+        const links = this.template.getElementsByClassName('navigation-link');
+
+        Array.prototype.map.call(links, link => link.addEventListener('click', this.clickLink));
+    }
+
+    clickLink(e) {
+        const linkRoute = e.target.attributes['route'];
+        if(linkRoute && linkRoute.value){
+            location.hash = '/' + linkRoute.value;
+        }
+    }
 };
