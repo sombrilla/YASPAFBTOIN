a214d0106affceecf72b498321d4b44314010439
Juan Beldi <juan.beldi@mediamonks.com>
2020-05-21 13:42:36 -0300

    Add demo components with base style

---
+++ components/navigation/navigation.js
@@ -0,0 +1,16 @@
+import { Component } from '../../lib/Component.js';
+
+export class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.props = {
+            test: 'Home',
+        }
+    }
+    
+    mounted() {
+        const links = this.template.getElementsByClassName('navigation-link');
+
+        Array.prototype.map.call(links, link => link.addEventListener('click', this.clickLink));
+    }
+};
